2227245751cc1c1cfcf1662f5d4ed68836d2b859
SeborExe <mix98@o2.pl>
2022-04-17 14:49:36 +0200

    Gun
    Gun has been added.
    Enemy script changes.

--- REKT/Assets/__Scripts/EnemyAI.cs
+++ REKT/Assets/__Scripts/EnemyAI.cs
@@ -11,6 +11,7 @@ public class EnemyAI : MonoBehaviour
     NavMeshAgent navMeshAgent;
 
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     private void Start()
     {
@@ -21,8 +22,38 @@ public class EnemyAI : MonoBehaviour
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
 
-        if (chaseRange >= distanceToTarget)
-            navMeshAgent.SetDestination(target.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+
+        else if (chaseRange >= distanceToTarget)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    void AttackTarget()
+    {
+        Debug.Log("Uhhh trafi³ mnie!");
     }
 
     private void OnDrawGizmosSelected()
